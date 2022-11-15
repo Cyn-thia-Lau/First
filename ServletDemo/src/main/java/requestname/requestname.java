87c69145841675433ec3358fb0a7e92283b01da4
CynthiaLau9 <118267323+CynthiaLau9@users.noreply.github.com>
2022-11-15 16:47:15 +0000

    Add files via upload

---
+++ ServletDemo/src/main/java/requestname/requestname.java
@@ -0,0 +1,52 @@
+package requestname;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class requestname
+ */
+@WebServlet("/requestname")
+public class requestname extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public requestname() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("Text/html"); //html web language
+		PrintWriter pw = response.getWriter();
+		
+		String n1 = request.getParameter("name"); //will return the value
+		pw.println("Welcome: " + n1);
+		
+	
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
